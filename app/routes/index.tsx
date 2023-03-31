37a2b1eb4c009d2eb6d97a832f73a86467c31c0b
Scott Wang <scottwang@Victorias-MacBook-Pro.local>
2023-03-31 08:55:04 -0700

    wip landing page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,65 +8,59 @@ export const meta: V2_MetaFunction = () => [{ title: "Remix Notes" }];
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <div className="relative sm:pb-16 sm:pt-8">
-        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-          <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
-            <div className="absolute inset-0">
-              <img
-                className="h-full w-full object-cover"
-                src="https://user-images.githubusercontent.com/1500684/158276320-c46b661b-8eff-4a4d-82c6-cf296c987a12.jpg"
-                alt="BB King playing blues on his Les Paul guitar"
-              />
-              <div className="absolute inset-0 bg-[color:rgba(27,167,254,0.5)] mix-blend-multiply" />
-            </div>
-            <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
-                <span className="block uppercase text-blue-500 drop-shadow-md">
-                  Blues Stack
-                </span>
-              </h1>
-              <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-                Check the README.md file for instructions on how to get this
-                project deployed.
-              </p>
-              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
-                {user ? (
-                  <Link
-                    to="/notes"
-                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-                  >
-                    View Notes for {user.email}
-                  </Link>
-                ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                    <Link
-                      to="/join"
-                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-blue-700 shadow-sm hover:bg-blue-50 sm:px-8"
-                    >
-                      Sign up
-                    </Link>
-                    <Link
-                      to="/login"
-                      className="flex items-center justify-center rounded-md bg-blue-500 px-4 py-3 font-medium text-white hover:bg-blue-600"
-                    >
-                      Log In
-                    </Link>
-                  </div>
-                )}
+    <main className="relative min-h-screen bg-white lg:flex lg:items-center lg:justify-center">
+      <div className="relative min-w-full sm:pb-16 sm:pt-8 font-mono">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 border-b-2">
+          <div className="flex flex-col">
+            <div className="flex items-end justify-between">
+              <div>
+                <h1 className="text-4xl sm:text-5xl lg:text-6xl">
+                  <span>
+                    modbot
+                  </span>
+                </h1>
               </div>
-              <a href="https://remix.run">
-                <img
-                  src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
-                  alt="Remix"
-                  className="mx-auto mt-16 w-full max-w-[12rem] md:max-w-[16rem]"
-                />
-              </a>
+              <div>
+                <span>
+                  AI-powered Twitch chat moderator
+                </span>
+              </div>
+            </div>
+            <div className="flex justify-start">
+              <Link to="/join" className="flex items-center justify-center bg-sky-300 rounded-md border border-transparent px-4 py-3 text-base font-medium shadow-sm sm:px-8">
+                Sign up
+              </Link>
+            </div>
+          </div>
+          
+        </div>
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="flex justify-end">
+            <div className="flex-1">
+              <div className="w-1/4">
+                <div className="border p-4 mt-4">
+
+                </div>
+                <div className="border p-4 mb-4">
+                  Try typing something...
+                </div>
+              </div>
+              <div className="w-1/4">
+                Results
+              </div>
+            </div>
+            <div className="border-l-2 max-w-sm">
+              <p>
+                modbot uses AI sentiment analysis to remove offensive messages that are hateful, violent, sexual, or promote self-harm.
+              </p>
+              <Link to="/join">
+                <u>Connect</u> your Twitch channel to set up modbot in minutes.
+              </Link>
             </div>
           </div>
         </div>
 
-        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
+        {/* <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
           <div className="mt-6 flex flex-wrap justify-center gap-8">
             {[
               {
@@ -134,7 +128,7 @@ export default function Index() {
               </a>
             ))}
           </div>
-        </div>
+        </div> */}
       </div>
     </main>
   );
